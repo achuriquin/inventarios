7759243d57cbee52396bda1f1e0f102345586c3d
achuriquin <dfghdfgdfg90@gmail.com>
2023-01-09 17:37:59 -0500

    inicio de datos solo main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'application/use_cases/frmPrincipal.dart';
+import 'package:inventarios/application/use_cases/frm_inicial.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+//ejecutar en la terminal "flutter pub add get"  es una dependencia que permite el uso de comandos abreviados 
+//ejecutar en la terminal "flutter pub add flutter_screenutil" es una dependencia que permite la muestra de objetos en pantalla con multiples resoluciones
+//ejecutar en la terminal "flutter pub add flutter_svg" esto permite que flutter pueda usar archivos svg
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder:(context, child) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(),
+        home: const frmInicial(),
+      ),
+    );
+  }
+}
+
